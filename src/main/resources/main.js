c8e9d40114b14a247e600294397665b5bfe7234b
Christian Westgaard <comlock@gmail.com>
2022-05-09 16:09:10 +0200

    Whitespace again

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -12,29 +12,29 @@ const REPO_ID_1 = `${app.name}.1`;
 
 
 function task() {
-  libContext.run({
-    repository: 'system-repo',
-    branch: 'master',
-    principals: ['role:system.admin']
-  }, () => {
+	libContext.run({
+		repository: 'system-repo',
+		branch: 'master',
+		principals: ['role:system.admin']
+	}, () => {
 		const createRepoParams1 = {
 			id: REPO_ID_1
 		};
 		log.debug(`createRepoParams1:${toStr(createRepoParams1)}`);
-    try {
-      libRepo.create(createRepoParams1);
-    } catch (e) {
-      if (e.class.name !== 'com.enonic.xp.repo.impl.repository.RepositoryAlreadyExistException') {
-        log.error(`e.class.name:${toStr(e.class.name)} e.message:${toStr(e.message)}`, e);
-      }
-    }
-    const connectParams1 = {
-      branch: 'master',
-      repoId: REPO_ID_1,
-      principals: ['role:system.admin']
-    };
-    log.debug(`connectParams1:${toStr(connectParams1)}`);
-    const connection1 = libNode.connect(connectParams1);
+		try {
+			libRepo.create(createRepoParams1);
+		} catch (e) {
+			if (e.class.name !== 'com.enonic.xp.repo.impl.repository.RepositoryAlreadyExistException') {
+				log.error(`e.class.name:${toStr(e.class.name)} e.message:${toStr(e.message)}`, e);
+			}
+		}
+		const connectParams1 = {
+			branch: 'master',
+			repoId: REPO_ID_1,
+			principals: ['role:system.admin']
+		};
+		log.debug(`connectParams1:${toStr(connectParams1)}`);
+		const connection1 = libNode.connect(connectParams1);
 
 		try {
 			connection1.delete('/1');
@@ -42,23 +42,23 @@ function task() {
 			log.error(`e.class.name:${toStr(e.class.name)} e.message:${toStr(e.message)}`, e);
 		}
 
-    const createNodeParams1 = {
-      _name: '1',
-      manycase: 'smallcase',
-      manyCase: 'camelCase',
-      MANYCASE: 'UPPERCASE'
+		const createNodeParams1 = {
+			_name: '1',
+			manycase: 'smallcase',
+			manyCase: 'camelCase',
+			MANYCASE: 'UPPERCASE'
 		};
 		log.debug(`createNodeParams1:${toStr(createNodeParams1)}`);
 		try {
 			const createdNode1 = connection1.create(createNodeParams1);
-      log.info(`createdNode1:${toStr(createdNode1)}`);
+			log.info(`createdNode1:${toStr(createdNode1)}`);
 		} catch (e) {
 			if (e.class.name !== 'com.enonic.xp.node.NodeAlreadyExistAtPathException') {
 				log.error(`e.class.name:${toStr(e.class.name)} e.message:${toStr(e.message)}`, e);
 			}
 		}
-    connection1.refresh();
-  }); // run
+		connection1.refresh();
+	}); // run
 } // task
 
 
